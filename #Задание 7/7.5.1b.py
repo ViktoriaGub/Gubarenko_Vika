b949b4ce21b4353c564bf7308be0d5c94af98346
ViktoriaGub <146134203+ViktoriaGub@users.noreply.github.com>
2023-10-29 13:39:39 -0700

    Delete #Задание 7 directory

--- #Задание 7/7.5.1b.py
+++
@@ -1,4 +0,0 @@
-a = [-2, -3, 7,  0, -3, -5, 2, -8, -4, 6]
-for i in range(len(a) - 1):
-    if a[i] < 0 and a[i+1] < 0:
-        print(a[i], a[i+1])
--- #Задание 7/7.5.2a.py
+++
@@ -1,6 +0,0 @@
-a = []
-for i in range(10):
-    n = int(input(f"Введите число {i+1}: "))
-    a.append(n)
-b=list(set(a))
-print(b)
