30cdc35906090a6e3db5507b7574b511a68cbd75
ViktoriaGub <146134203+ViktoriaGub@users.noreply.github.com>
2023-12-03 17:26:24 +0300

    Delete #Задание 10/#Задание 10_1.py

--- #Задание 10/#Задание 10_1.py
+++
@@ -1,5 +0,0 @@
-input_file_path = "C:\Users\Admin\Desktop\Губаренко Вика\#Задание 10\Губаренко_Виктория_Витальевна_У-234_vvod.txt"
-output_file_path = "C:\Users\Admin\Desktop\Губаренко Вика\#Задание 10\Губаренко_Виктория_Витальевна_У-234_vivod.txt"
-with open(input_file_path, 'r', encoding='utf-8') as file_in, open(output_file_path, 'w', encoding='utf-8') as file_out:
-    for line in file_in:
-        file_out.write(processed_data + '\n')
